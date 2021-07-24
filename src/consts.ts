b3d875988395703fd53e2ce61decc8a6573d9dbd
VictoriaZ04 <vzhang0779@gmail.com>
2021-07-24 04:01:54 -0500

    initial commit

---
+++ src/consts.ts
@@ -0,0 +1,22 @@
+const fortniteClassId = 8032;
+
+const interestingFeatures = [
+  'game_info',
+  'match_info'
+];
+
+const windowNames = {
+  inGame: 'in_game',
+  desktop: 'desktop'
+};
+
+const hotkeys = {
+  toggle: 'showhide'
+};
+
+export {
+  fortniteClassId,
+  interestingFeatures,
+  windowNames,
+  hotkeys
+}
